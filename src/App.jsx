fde6e9ccee050676fc59eeb29ac9964ce591deaf
Prathamesh Shakewar <shakewarprathmesh@gmail.com>
2026-05-07 19:21:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function Layout({ children }) {
     try {
       const token = localStorage.getItem('token');
       if (token) {
-        await axios.post(' http://127.0.0.1:8000/api/logout/', {}, {
+        await axios.post('https://tourist-backend-j0qo.onrender.com/api/logout/', {}, {
           headers: {
             'Authorization': `Token ${token}`
           }
