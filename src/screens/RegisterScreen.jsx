8aeb4d9a903e350dde00614139d1667b57a835cf
Prathamesh Shakewar <shakewarprathmesh@gmail.com>
2026-05-07 19:28:43 +0530

    Update API endpoints for user registration and login

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -14,13 +14,13 @@ export default function RegisterScreen() {
     
     try {
       // 1. Register the user
-      await axios.post(' http://127.0.0.1:8000/api/register/', {
+      await axios.post(' https://tourist-backend-j0qo.onrender.com/api/register/', {
         username,
         password
       });
       
       // 2. Automatically log them in after registration
-      const loginResponse = await axios.post(' http://127.0.0.1:8000/api/login/', {
+      const loginResponse = await axios.post(' https://tourist-backend-j0qo.onrender.com/api/login/', {
         username,
         password
       });
